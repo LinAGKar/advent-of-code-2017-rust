df03b2c1167920f0217cd09d1877cdb152bee8a4
Linus <Kardell>
2017-12-09 14:03:51 +0100

    Day 8 part 1

---
+++ advent_of_code_2017_8a/src/main.rs
@@ -0,0 +1,38 @@
+extern crate regex;
+
+use regex::Regex;
+use std::collections::HashMap;
+use std::io;
+use std::io::Read;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let mut regs = HashMap::new();
+    let re = Regex::new(r"^(\w+) (inc|dec) (-?\d+) if (\w+) (>|<|==|!=|>=|<=) (-?\d+)$").unwrap();
+    for line in input.lines() {
+        if let Some(caps) = re.captures(line) {
+            let comp_val: i64 = caps[6].parse().expect("Failed to parse comparison value");
+            let comp_reg = match regs.get(&caps[4]) { Some(x) => *x, None => 0 };
+            if match &caps[5] {
+                ">" => comp_reg > comp_val,
+                "<" => comp_reg < comp_val,
+                "==" => comp_reg == comp_val,
+                "!=" => comp_reg != comp_val,
+                ">=" => comp_reg >= comp_val,
+                "<=" => comp_reg <= comp_val,
+                _ => panic!(),
+            } {
+                let reg = regs.entry(caps[1].to_string()).or_insert(0);
+                let val: i64 = caps[3].parse().expect("Failed to parse operation value");
+                match &caps[2] {
+                    "dec" => { *reg -= val; }
+                    "inc" => { *reg += val; }
+                    _ => { panic!(); },
+                }
+            }
+        }
+    }
+
+    println!("{}", regs.values().max().unwrap());
+}
