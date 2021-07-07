e0d4a86fafa4fbd6d7733fc993d4b4dee6d2295e
Linus Kardell <linus.kardell@gmail.com>
2021-07-07 21:42:57 +0200

    Slight optimization for day 17 part 2

--- day17b/src/main.rs
+++ day17b/src/main.rs
@@ -4,19 +4,14 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
     let input: usize = input.trim().parse().unwrap();
+
     let mut pos = 0;
-    let mut counter = 1;
-    let mut zero_pos = 0;
     let mut after_zero = 0;
-    while counter <= 50000000 {
-        pos = (pos + input) % counter + 1;
-        if pos == (zero_pos + 1) % counter {
-            after_zero = counter;
+    for i in 1..=50000000 {
+        pos = (pos + input) % i + 1;
+        if pos == 1 {
+            after_zero = i;
         }
-        if pos <= zero_pos {
-            zero_pos += 1;
-        }
-        counter += 1;
     }
     println!("{}", after_zero);
 }
